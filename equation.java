e48372dfa9eb7aa4b76ee70579456eb83505db2a
Vladimir Malyavka <malyavkava@gmail.com>
2016-10-09 23:24:11 +0300

    Add files via upload
    Ввод через Scanner

--- equation.java
+++ equation.java
@@ -1,37 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.Scanner;
 public class equation {
-    public static void main(String[] args) throws IOException {
-        boolean checkA = true;
-        boolean checkB = true;
+    public static void main(String[] args) {
         System.out.println("Решение уравнения \"a*x+b=0\"");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (checkA) {
-            try {
-                System.out.println("Введите \"a\":");
-                float a = Float.parseFloat(reader.readLine());
-                if (a != 0) {
-                    while (checkB) {
-                        try {
-                            System.out.println("Введите \"b\":");
-                            float b = Float.parseFloat(reader.readLine());
-                            float x = (-b) / a;
-                            System.out.println("\"x\" равен " + x);
-                            checkB = false;
-                            checkA = false;
-                        } catch (NumberFormatException e) {
-                            System.out.println("\"b\" должно быть числом!");
-                        }
-                    }
-                } else {
-                    System.out.println("\"x\" равен любому значению");
-                    checkA = false;
-                }
-            } catch (NumberFormatException e) {
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
+        float a, b;
+        while (true) {
+            System.out.println("Введите \"a\" и нажмите Enter:");
+            if (sc.hasNextFloat()) {
+                a = sc.nextFloat();
+                System.out.println("Ввол \"a\" закончен");
+                break;
+            } else {
                 System.out.println("\"a\" должно быть числом!");
+                sc.next();
             }
+        }
+        if (a == 0) {
+            System.out.println("\"x\" равен любому значению");
+        } else {
+            while (true) {
+                System.out.println("Введите \"b\" и нажмите Enter:");
+                if (sc.hasNextFloat()) {
+                    b = sc.nextFloat();
+                    System.out.println("Ввод \"b\" закончен");
+                    break;
+                } else {
+                    System.out.println("\"b\" должно быть числом!");
+                    sc.next();
+                }
+            }
+            float x = (-b) / a;
+            System.out.println("\"x\" равен " + x);
         }
     }
 }
