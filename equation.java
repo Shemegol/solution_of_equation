b160a75c00714ab9e0eb675b1a3dd7792d76f3dc
Vladimir Malyavka <malyavkava@gmail.com>
2016-10-10 18:23:32 +0300

    Добавил метод ввода чисел с консоли

--- equation.java
+++ equation.java
@@ -1,36 +1,36 @@
+package equation;
+
 import java.util.Scanner;
+
 public class equation {
+    private static Scanner sc = new Scanner(System.in).useDelimiter("[\r\n]+");
+
     public static void main(String[] args) {
         System.out.println("Решение уравнения \"a*x+b=0\"");
-        Scanner sc = new Scanner(System.in).useDelimiter("[\r\n]+");
-        float a, b;
-        while (true) {
-            System.out.println("Введите \"a\" и нажмите Enter:");
-            if (sc.hasNextFloat()) {
-                a = sc.nextFloat();
-                System.out.println("Ввол \"a\" закончен");
-                break;
-            } else {
-                System.out.println("\"a\" должно быть числом!");
-                sc.next();
-            }
-        }
+        float a = inputNum("a");
         if (a == 0) {
             System.out.println("\"x\" равен любому значению");
         } else {
-            while (true) {
-                System.out.println("Введите \"b\" и нажмите Enter:");
-                if (sc.hasNextFloat()) {
-                    b = sc.nextFloat();
-                    System.out.println("Ввод \"b\" закончен");
-                    break;
-                } else {
-                    System.out.println("\"b\" должно быть числом!");
-                    sc.next();
-                }
-            }
+            float b = inputNum("b");
             float x = (-b) / a;
             System.out.println("\"x\" равен " + x);
         }
+        sc.close();
+    }
+
+    private static float inputNum(String n) { //Ввод числа с консоли. Как параметр указываеся имя константы.
+        float k;
+        while (true) {
+            System.out.println("Введите \"" + n + "\" и нажмите Enter:");
+            if (sc.hasNextFloat()) {
+                k = sc.nextFloat();
+                System.out.println("Ввол \"" + n + "\" закончен");
+                break;
+            } else {
+                System.out.println("\"" + n + "\" должно быть числом!");
+                sc.next();
+            }
+        }
+        return k;
     }
 }
